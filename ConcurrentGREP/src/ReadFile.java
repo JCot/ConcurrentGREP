e7305faba5ab42d5a5470534d3c7596190466c50
Justin Cotner <jdc9622@rit.edu>
2014-10-29 10:32:43 -0400

    Blah

---
+++ ConcurrentGREP/src/ReadFile.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.Callable;
+
+
+public class ReadFile implements Callable{
+
+	@Override
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
